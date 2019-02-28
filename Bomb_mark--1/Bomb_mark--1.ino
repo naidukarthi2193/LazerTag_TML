03823285fbecb8281ad30cd8883b7368e9851ff8
KarthikRaj <naidukarthi2193@gmail.com>
2019-02-28 19:10:10 +0530

    final

--- Bomb_mark--1/Bomb_mark--1.ino
+++ Bomb_mark--1/Bomb_mark--1.ino
@@ -13,22 +13,25 @@ int buzzerdivide=1;                         //buzzer delay time
 unsigned long currentmil;                   //track time 
 int disp=0 ;
 int dsec;
+int firstcount=0;
 TM1637Display display(CLK, DIO);  //set up the 4-Digit Display.
- 
+ void buzzerfun();
 void setup() {
   Serial.begin(9600); 
   pinMode(buzzer,OUTPUT);
-  pinMode(button,INPUT);
-  pinMode(defbut,INPUT);
+  pinMode(button, INPUT_PULLUP);
   display.setBrightness(0x0a);  //set the diplay to maximum brightness
 }
 
 void loop() {
-  Serial.println(butcount);
+  
+  Serial.println(Serial.read());
   currentmil=millis();                      //get current time
-  if(digitalRead(button)==HIGH)
-  butcount=1;
-  if(btime+currentmil>currentmil  && butcount==1)   //if button is pressed and btime is zero
+  if(digitalRead(button)==LOW){
+    butcount++;
+  }
+ 
+  if(btime+currentmil>currentmil  && butcount>0)   //if button is pressed and btime is zero
   {
     buzzerfun();                             //buzzer single beep function
     delay(buuzertime/buzzerdivide);          //time difference between 2 beeps
@@ -46,17 +49,36 @@ void loop() {
     Serial.print(bsec);
     Serial.println("");
     Serial.print(disp);
-if(digitalRead(defbut) && disp>10)
+    Serial.print(Serial.read());
+if(Serial.read()>0 && disp>10 && firstcount != 1)
 {
+  
   for(int i=10;i>0;i--)
   {
     display.showNumberDec(i);
     delay(1000);
+    Serial.print("btime");
+    Serial.print(btime);
+    if(i<2)
+    firstcount=1;
   }
-  btime=0;
+   
+  delay(100);
 }
+
+delay(100);
+Serial.print(btime);
+Serial.println("-1");
+if(Serial.read()>50 && btime!=0 && firstcount==1)
+{
+  Serial.println("loop");
+  delay(100);
+  btime=0;
+  }
     
-  } 
+//  else{
+//    display.showNumberDec(0); 
+//  }
   if(btime==0)
   {
     digitalWrite(buzzer,HIGH);
@@ -64,7 +86,7 @@ if(digitalRead(defbut) && disp>10)
     digitalWrite(buzzer,LOW);
     delay(100000);
   }
-}
+}}
 void buzzerfun()                            //funtion for short 1 beep
 {
   digitalWrite(buzzer,HIGH);
