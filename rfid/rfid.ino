5992173352bd85fc260b4eb2535e041ad08251e9
KarthikRaj <naidukarthi2193@gmail.com>
2019-03-07 01:44:20 +0530

    open rfid

--- rfid/rfid.ino
+++ rfid/rfid.ino
@@ -1,31 +1,38 @@
+#include <MFRC522.h>
+#include <SPI.h>
 #include <TM1637Display.h>
-
-const int CLK = 9; //Set the CLK pin connection to the display
-const int DIO = 8; //Set the DIO pin connection to the display
-int button =13;                             //button for start of timer
+#define SS_PIN 10
+#define RST_PIN 9
+MFRC522 mfrc522(SS_PIN, RST_PIN);
+ int co=1;
+ const int CLK = 6; 
+const int DIO = 7;
+int button =4;
 int defbut =2;
 int butcount=0;                             //buttoncounter
 long int btime=120000;                       //Time for timer  
 long int bmin=0,bsec=0;                       
-int buzzer=12;                              //buzzer pin 
+int buzzer=2;                              //buzzer pin 
 int buuzertime=2000;
 int buzzerdivide=1;                         //buzzer delay time
 unsigned long currentmil;                   //track time 
 int disp=0 ;
 int dsec;
 int firstcount=0;
-TM1637Display display(CLK, DIO);  //set up the 4-Digit Display.
- void buzzerfun();
+TM1637Display display(CLK, DIO);
+void buzzerfun();  
 void setup() {
-  Serial.begin(9600); 
+  Serial.begin(9600); // Initialize serial communications with the PC
+  SPI.begin();      // Init SPI bus
+  mfrc522.PCD_Init(); // Init MFRC522 card
   pinMode(buzzer,OUTPUT);
   pinMode(button, INPUT_PULLUP);
-  display.setBrightness(0x0a);  //set the diplay to maximum brightness
+  display.setBrightness(0x0a); 
+//  Serial.println("Scan PICC to see UID and type...");
 }
 
 void loop() {
-  
-  Serial.println(Serial.read());
+
   currentmil=millis();                      //get current time
   if(digitalRead(button)==LOW){
     butcount++;
@@ -34,7 +41,7 @@ void loop() {
   if(btime+currentmil>currentmil  && butcount>0)   //if button is pressed and btime is zero
   {
     buzzerfun();                             //buzzer single beep function
-    delay(buuzertime/buzzerdivide);          //time difference between 2 beeps
+    delay(1000);          //time differece between 2 beeps
     bmin=btime/60000;                        //conversion to minutes
     bsec=btime-bmin*60000;
     dsec=bsec/1000;
@@ -44,50 +51,36 @@ void loop() {
     btime=btime-1000;
                        //time reduces every second
     display.showNumberDec(disp);
-    Serial.print(bmin);
-    Serial.print(" : ");
-    Serial.print(bsec);
-    Serial.println("");
-    Serial.print(disp);
-    Serial.print(Serial.read());
-if(Serial.read()>0 && disp>10 && firstcount != 1)
-{
-  
-  for(int i=10;i>0;i--)
-  {
-    Serial.read();
-    display.showNumberDec(i);
-    delay(1000);
-    Serial.print("btime");
-    Serial.print(btime);
-    if(i<2)
-    firstcount=1;
-  }
-   
-  delay(100);
-}
+    while( mfrc522.PICC_IsNewCardPresent()){
+      display.showNumberDec(co);
+      co++;
+//      delay(1000);
+      
+    }
 
-delay(100);
-Serial.print(btime);
-Serial.println("-1");
-if(Serial.read()>50 && btime!=0 && firstcount==1)
-{
-  Serial.println("loop");
-  delay(100);
-  btime=0;
-  }
     
-//  else{
-//    display.showNumberDec(0); 
-//  }
-  if(btime==0)
+    if(co>10)
+    btime=0;
+//    else
+//    co=0;
+  }
+   if(btime==0)
   {
     digitalWrite(buzzer,HIGH);
     delay(10000);
     digitalWrite(buzzer,LOW);
     delay(100000);
   }
-}}
+  
+//   Look for new cards
+//  if ( mfrc522.PICC_IsNewCardPresent()) {
+//    Serial.print("Defuse : ");
+//    Serial.print(count);
+//    count++;
+//    return;
+//  }
+
+}
 void buzzerfun()                            //funtion for short 1 beep
 {
   digitalWrite(buzzer,HIGH);
